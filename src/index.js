125965b389753bc20e9e91c2e5f6ea90782a5d5e
bearBenjamin <git@github.com:bearBenjamin/cinemaddict.git>
2025-08-29 10:57:21 +0300

    feat: add coding index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function reverse(/* n */) {
-  throw new Error('Not implemented');
+module.exports = function reverse(n) {
+  const nStr = Math.abs(n).toString();
+  const nStrArray = nStr.split('');
+  const resultArray = [];
+
+  for (let i = nStrArray.length - 1; i >= 0; i -= 1) {
+    resultArray.push(nStrArray[i]);
+  }
+
+  const result = Number(resultArray.join(''));
+  return result;
 };
